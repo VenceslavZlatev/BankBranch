e2f8f012a600d934871829178ebd12e5009d61bc
Venceslav Zlatev <35622512+VenceslavZlatev@users.noreply.github.com>
2023-01-18 21:54:11 +0200

    Add files via upload

---
+++ BankBranch/BankBranch/Account.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankBranch
+
+{
+    abstract class Account
+    {
+        public readonly string name;
+        public readonly string password;
+        public readonly string ID;
+        public double balance;
+        protected string type;
+        public double ammount;
+        public abstract bool deposit(double amount);
+
+        public abstract bool withdraw(double amount);
+
+        public double getBalance()
+        {
+            return balance;
+        }
+        public string getAccType()
+        {
+            string actype;
+            actype = Convert.ToString(Console.ReadLine());
+            return actype;
+        }
+        public void printAccount()
+        {
+
+            Console.WriteLine("Name : " + name);
+            Console.WriteLine("Password : " + password);
+            Console.WriteLine("Balance :" + balance);
+        }
+        public Account()
+        {
+
+        }
+        public Account(string name, string password, double balance)
+        {
+            this.name = name;
+            this.password = password;
+            this.balance = balance;
+        }
+    }
+}
+
---
+++ BankBranch/BankBranch/Program.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Reflection.Emit;
+
+namespace BankBranch
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input;
+            
+            Bank bank = new Bank();
+            Console.WriteLine("Bank Branch");
+            while (true)
+            {
+                Console.WriteLine("\nChoose an option:");
+                Console.WriteLine("1. Create account");
+                Console.WriteLine("2. Show account information");
+                Console.WriteLine("3. Deposit");
+                Console.WriteLine("4. Withdraw");
+                Console.WriteLine("5. Delete Account");
+                Console.WriteLine("6. Clear screen");
+                Console.WriteLine("7. Exit");
+                object ob1 = Console.ReadLine();
+                input = Convert.ToString(ob1);
+
+                if (input == "1")
+                {
+                    Console.WriteLine("Enter account Type :");
+                    bank.create_account();
+
+                }
+                else if (input == "2")
+                {
+                    Console.Write("Enter account Number :");
+                    bank.showInfo();
+                }
+                else if (input == "3")
+                {
+                    Console.WriteLine("Enter Account Id: ");
+                    bank.deposit();
+                }
+                else if (input == "4")
+                {
+                    Console.WriteLine("Enter Account Id: ");
+                    bank.withdraw();
+                }
+                else if (input == "5")
+                {
+                    bank.deleteAcc();
+                }
+                else if (input == "6")
+                {
+                    Console.Clear();
+                }
+                else if (input == "7")
+                {
+                    Environment.Exit(0);
+                }
+                Console.ReadKey();
+
+
+            }
+
+        }
+
+    }
+}
+
